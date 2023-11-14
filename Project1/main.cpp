9365cec5c97f114281755829decff7b8ff5beee8
Jeremy <jeremylc153@gmail.com>
2023-11-14 12:00:00 -0500

    initial commit for C++ Udemy Course

---
+++ Project1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num{100};
+    cout << "Hello windows command line" << endl;
+    cout << "Enter your favorite number :";
+    cin >> num;
+    cout << "You favorite number is " << num << endl;
+    return 0;
+}
