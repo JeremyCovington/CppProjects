a80865a2b5eea5c5ae0b71ec3250988aff889099
Jeremy <jeremylc153@gmail.com>
2023-11-19 12:00:00 -0500

    name C++ exercise

---
+++ Section 1/nameExercise/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+     cout << "Jeremy Covington" << endl;
+     cout << endl
+          << "Jeremy" << endl;
+     cout << "Covington" << endl;
+     cout << "Age: " << 25 << endl;
+     cout << endl
+          << "--- End of Program ---" << endl;
+
+     // system("pause"); //don't do this -avoid system() commands!
+     cin.get();
+
+     return 0;
+}
