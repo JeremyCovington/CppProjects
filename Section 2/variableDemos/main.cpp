e9bcc8357a82f22f91aff7ecfc70d13fd71aadae
Jeremy <jeremylc153@gmail.com>
2023-11-20 12:00:00 -0500

    commit for variable lesson

---
+++ Section 2/variableDemos/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Literals
+    cout << "Hello!!" << endl;
+    cout << 33 << endl;
+    cout << "I'm " << 25 << " years of age." << endl;
+    return 0;
+}
