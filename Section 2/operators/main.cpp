b610ac38b0393b4544b0eaae353a6b9b848c1d29
Jeremy <jeremylc153@gmail.com>
2023-11-21 12:00:00 -0500

    commit for operator lesson

---
+++ Section 2/operators/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int length = 4;
+    int width = 5;
+    int area = length * width;
+    int x = 4;
+    cout << "x is: " << x << endl;
+    cout << "Negative x is: " << -x << endl;
+    cout << "x times 5 is: " << x * 5 << endl;
+    cout << "x plus 5 is: " << x + 5 << endl;
+    cout << "x minus 5 is: " << x - 5 << endl;
+    cout << "x divided by 5 is: " << x / 5 << endl;
+    cout << "The area is: " << area << endl;
+    return 0;
+}
